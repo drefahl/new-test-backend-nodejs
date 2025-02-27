c1084f606e964a3b759e6ad27648f86cc26ba140
Caua Drefahl <cauadrefahl@gmail.com>
2025-02-27 18:11:27 -0300

    feat(swagger): integrate Swagger for API documentation and add ApiProperty decorators

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly title: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly description: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly owner: string;
 }
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,22 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly title: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly description: string;
 
   @IsNumber()
+  @ApiProperty()
   readonly price: number;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly category: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly owner: string;
 }
